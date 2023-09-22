cf0b7f1747feec954a8cb5b00f609b9cd7b93e02
Amber Hewett <hewettabc@gmail.com>
2023-09-22 14:33:54 -0400

    can perform commands of /bin and /usr/bin paths

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+/*
+https://stackoverflow.com/questions/7656549/understanding-requirements-for-execve-and-setting-environment-vars
+
+*/
 #include <stdio.h>
 #include <string.h> // only specifies we must use system calls for file access 
 #include <sys/types.h>
@@ -15,81 +19,90 @@ int main(void){
   char line[MAXCHAR];
   char* cmd;
 
-  printf("@ "); 
-  fgets(line, MAXCHAR, stdin);
-  cmd = line;
-  if((line[0] == '&') && (line[1] == ' ')){
-    bkgFlag = 1;
-    cmd = line + 2;
-  }
-
-  int wtspace=0;
-  for(char* tmp = cmd; *tmp != '\n'; tmp++){
-    if(*tmp == ' '){
-      wtspace++;
+  while(1){
+    printf("@ "); 
+    fgets(line, MAXCHAR, stdin);
+    cmd = line;
+    if((line[0] == '&') && (line[1] == ' ')){
+      bkgFlag = 1;
+      cmd = line + 2;
     }
-  }
-  printf("wtspace=%d\n", wtspace);
-  int arrLength = wtspace+2;
-  char *tokenArr[arrLength]; // need extra element for NULL, if there is 1 whitespace there is 2 tokens
 
-  tokenArr[arrLength-1] = NULL;
-  char *token;
-  int i = 0;  
-  while(i < arrLength-1){
-    printf("i=%d\n", i);
-    if(i==0){
-      token = token_maker(cmd);
+    int wtspace=0;
+    for(char* tmp = cmd; *tmp != '\n'; tmp++){
+      if(*tmp == ' '){
+        wtspace++;
+      }
     }
+    // printf("wtspace=%d\n", wtspace);
+    int arrLength = wtspace+2;
+    char *tokenArr[arrLength]; // need extra element for NULL, if there is 1 whitespace there is 2 tokens
+    tokenArr[arrLength-1] = NULL;
+    char *token;
+    int i = 0;  
+    while(i < arrLength-1){
+      // printf("i=%d\n", i);
+      if(i==0){
+        token = token_maker(cmd);
+        for(char* tmp = token; *tmp != '\0'; tmp++){
+          }
+      }
+      else{
+      token = token_maker(0);
+      if(token == 0){break;} // no more tokens to parse, after breaking i = num of toks 
+      }
+      //puts(token);
+      tokenArr[i] = token;
+      i++;
+    }
+
+    if(strcmp(line, "exit") == 0){
+      printf("internal exiting cmd\n");
+      return 0;
+    }
+
     else{
-    token = token_maker(0);
-    if(token == 0){break;} // no more tokens to parse, after breaking i = num of toks 
-    }
-    puts(token);
-    tokenArr[i] = token;
-    i++;
-  }
+      pid = fork();
+      if (pid == 0){ // if child process
+        int error;
+        const char *bin = "/bin/";
+        const char *usrbin = "/usr/bin/";
+        char path[MAXCHAR];
 
-  for(int i=0; i< arrLength; i++){
-    printf("token%d=%s\n", i, tokenArr[i]);
-  }
+        strcpy(path, bin);
+        strcat(path, tokenArr[0]);
+        // printf("path %s\n", path);
 
-  // else if(strcmp(line, "exit") == 0){
-  //   printf("internal exit cmd\n");
-  //   killShell = 1;
-  //   //printf("exit: killShell == %d", killShell);
-    
-  // if(strcmp(line, "more") == 0){
-  //   printf("internal more cmd\n");
-  // }
 
-  // }
-  //else{
-    //pid = fork();
-    //if (pid == 0){ // if child process
-      //int error;
-      // if((error = execve("/bin", tokenArr, 0)) == -1){
-      //   if((error = execve("/usr/bin", tokenArr, 0)) == -1){
-      //     if((error = execve(cmd, tokenArr, 0)) == -1){
-      //       printf("command not found");
-      //     }
+        if((error = execve(path, tokenArr, 0)) == -1){ // try bin path
+
+          strcpy(path, usrbin);
+          strcat(path, tokenArr[0]);
+          printf("path %s\n", path);
+
+          if((error = execve(path, tokenArr, 0)) == -1){ // try usrbin path
+            if((error = execve(cmd, tokenArr, 0)) == -1){
+              printf("command not found\n");
+              return 0;
+            }
+          }
+        }
+
+      //   else{
+      //    if(bkgFlag == 1){
+      //     printf("background cmd\n");
       //   }
       // }
-    //   else{
-    //    if(bkgFlag == 1){
-    //     printf("background cmd completed\n");
-    //   }
-    // }
-    //}
-    //else { // if parent process
-    //   if(bkgFlag == 1){
-    //   }
-    //   else{
-    //     wait(0);
-    //   }
-    // }
-  //}
-  return 0;
+      }
+
+      else { // if parent process
+        if(bkgFlag == 1){}
+        else{
+          wait(0);
+        }
+      }
+    }
+  }
 }
 
 char *token_maker(char *s){ // first time token_maker called, s is the raw input from get
